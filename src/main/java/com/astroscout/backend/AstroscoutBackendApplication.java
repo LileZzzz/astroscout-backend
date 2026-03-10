6cc0cb556195676189fa002c644683b2b3e57be5
lilez <zhang.lile@northeastern.edu>
2026-03-10 12:11:23 -0400

    Initial AstroScout backend skeleton

---
+++ src/main/java/com/astroscout/backend/AstroscoutBackendApplication.java
@@ -0,0 +1,13 @@
+package com.astroscout.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroscoutBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstroscoutBackendApplication.class, args);
+	}
+
+}
